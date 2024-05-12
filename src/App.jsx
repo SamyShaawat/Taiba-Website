ae0ca3da8cbe40820d55f1d560523261ecf7870e
SamyShaawat <samy.shaawat@gmail.com>
2024-05-12 23:32:29 +0300

    add footer and complete navbar without collapse

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
 import About from "./pages/about/About.jsx";
 import Projects from "./pages/projects/Projects.jsx";
 import Clients from "./pages/clients/Clients.jsx";
 import Certificates from "./pages/certificates/Certificates.jsx";
 import Contact from "./pages/contact/Contact.jsx";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-      <About />
-      <Projects />
-      <Clients />
-      <Certificates />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/our-clients" element={<Clients />} />
+        <Route path="/certificates" element={<Certificates />} />
+        <Route path="/contact-us" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,70 @@
 import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo */}
+        <a href="/" className="navbar-logo">
+          <img src="../src/images/logo.png" className="navbar-logo-image" />
+        </a>
+        {/* Menu Items */}
+        <ul className="nav-menu">
+          <li
+            className={`nav-item ${
+              window.location.pathname === "/" ? "selected" : ""
+            }`}
+          >
+            <a href="/" className="nav-links">
+              Home
+            </a>
+          </li>
+          <li
+            className={`nav-item ${
+              window.location.pathname === "/about-us" ? "selected" : ""
+            }`}
+          >
+            <a href="/about-us" className="nav-links">
+              About Us
+            </a>
+          </li>
+          <li
+            className={`nav-item ${
+              window.location.pathname === "/projects" ? "selected" : ""
+            }`}
+          >
+            <a href="/projects" className="nav-links">
+              Projects
+            </a>
+          </li>
+          <li
+            className={`nav-item ${
+              window.location.pathname === "/our-clients" ? "selected" : ""
+            }`}
+          >
+            <a href="/our-clients" className="nav-links">
+              Our Clients
+            </a>
+          </li>
+          <li
+            className={`nav-item ${
+              window.location.pathname === "/certificates" ? "selected" : ""
+            }`}
+          >
+            <a href="/certificates" className="nav-links">
+              Certificates
+            </a>
+          </li>
+        </ul>
+        {/* Contact Us */}
+        <a href="/contact-us" className="nav-contact">
+          <span>Contact Us</span>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </a>
+      </div>
+    </nav>
+  );
 };
-
 export default Navbar;
